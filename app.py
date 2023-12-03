34f75ed4c8f08b85b873a0d8f59cd51b05658446
dfsorioc <dfsorioc@gmail.com>
2023-12-03 13:15:29 -0500

    Modificado 3

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template
 import pandas as pd
-from openpyxl import load_workbook
 
 app = Flask(__name__)
 
-datos= pd.read_excel('02122023 Arquitectura Curricular - Proyecto de Formación.xlsx', sheet_name='TIME')
+# Cargar datos desde el archivo Excel
+datos = pd.read_excel('02122023 Arquitectura Curricular - Proyecto de Formación.xlsx', sheet_name='TIME')
 
 @app.route('/')
 def header():
+    # Renderizar el encabezado y la introducción
     with open('templates/header.html', 'r') as header_file:
         header_html = header_file.read()
 
     with open('templates/introduccion.html', 'r') as intro_file:
         intro_html = intro_file.read()
 
-    # Debes retornar algo aquí, como el encabezado y la introducción
     return render_template('header.html', header=header_html, intro=intro_html)
 
 @app.route('/estructura')
 def mostrar_estructura():
-    # Lee el contenido de estructura.html
+    # Leer el contenido de estructura.html
     with open('templates/estructura.html', 'r') as estructura_file:
         estructura_html = estructura_file.read()
 
@@ -51,7 +51,10 @@ def mostrar_estructura():
 
 @app.route('/fase1')
 def fase1():
-    # Lee el contenido de fase1.html
+    """
+    Muestra la información correspondiente a la fase 1.
+    Lee el contenido de fase1.html y lo renderiza en la plantilla 'fase1.html'.
+    """
     with open('templates/fase1.html', 'r') as fase1_file:
         fase1_html = fase1_file.read()
 
@@ -59,7 +62,10 @@ def fase1():
 
 @app.route('/fase2')
 def fase2():
-    # Lee el contenido de fase2.html
+    """
+    Muestra la información correspondiente a la fase 2.
+    Lee el contenido de fase2.html y lo renderiza en la plantilla 'fase2.html'.
+    """
     with open('templates/fase2.html', 'r') as fase2_file:
         fase2_html = fase2_file.read()
 
@@ -67,7 +73,10 @@ def fase2():
 
 @app.route('/fase3')
 def fase3():
-    # Lee el contenido de fase3.html
+    """
+    Muestra la información correspondiente a la fase 3.
+    Lee el contenido de fase3.html y lo renderiza en la plantilla 'fase3.html'.
+    """
     with open('templates/fase3.html', 'r') as fase3_file:
         fase3_html = fase3_file.read()
 
@@ -75,7 +84,10 @@ def fase3():
 
 @app.route('/trimestre1')
 def trimestre1():
-    # Lee el contenido de trimestre1.html
+    """
+    Muestra la información correspondiente al trimestre 1.
+    Lee el contenido de trimestre1.html y lo renderiza en la plantilla 'trimestre1.html'.
+    """
     with open('templates/trimestre1.html', 'r') as trimestre1_file:
         trimestre1_html = trimestre1_file.read()
 
