d0a6fb539fb3f690700b8cda04d63ba6e81f2488
dfsorioc <dfsorioc@gmail.com>
2023-12-09 09:47:10 -0500

    Actualización de código

--- js/diagrama_barras.js
+++ js/diagrama_barras.js
@@ -1,71 +1,48 @@
-// Datos para el gráfico de barras
-const dataBar = {
-    labels: ['A', 'B', 'C', 'D'],
-    datasets: [{
-      label: 'Ejemplo de Datos para Barras',
-      data: [12, 19, 3, 5],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
-  
-  // Configuración para el gráfico de barras
-  const configBar = {
-    type: 'bar',
-    data: dataBar,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+// Aquí va tu código de Chart.js para generar los gráficos
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Datos para el primer gráfico de barras
+    const dataBar1 = {
+        labels: ['A', 'B', 'C', 'D'],
+        datasets: [{
+            label: 'Ejemplo de Datos para Barras 1',
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    // Configuración para el primer gráfico de barras
+    const configBar1 = {
+        type: 'bar',
+        data: dataBar1,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Formación Directa' // Título del gráfico de barras 1
+                }
+            }
         }
-      }
-    }
-  };
-  
-  // Dibujar gráfico de barras
-  const ctxBar = document.getElementById('barras').getContext('2d');
-  new Chart(ctxBar, configBar);
-  
-  // Datos para el gráfico de pastel
-  const dataPie = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green'],
-    datasets: [{
-      label: 'Ejemplo de Datos para Pastel',
-      data: [12, 19, 3, 5],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
-  
-  // Configuración para el gráfico de pastel
-  const configPie = {
-    type: 'pie',
-    data: dataPie
-  };
-  
-  // Dibujar gráfico de pastel
-  const ctxPie = document.getElementById('pastel').getContext('2d');
-  new Chart(ctxPie, configPie);
-  
+    };
+
+    // Dibujar el primer gráfico de barras
+    const ctxBar1 = document.getElementById('barras1').getContext('2d');
+    new Chart(ctxBar1, configBar1);
+});
